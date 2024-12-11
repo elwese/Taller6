991039762f30931ec7abab2a9d0d1c8778543bb6
jeloarte <jeloarte@espol.edu.ec>
2024-12-11 16:05:06 -0500

    Update Empleado.java

--- src/Empleado.java
+++ src/Empleado.java
@@ -1,44 +1,51 @@
-public class Empleado {
+public abstract class Empleado {
     private String nombre;
+    private String genero;
     private double salarioBase;
     private int horasTrabajadas;
     private String departamento;
     private double tarifaHora;
+    
 
     public Empleado(){}
-    public Empleado(String nombre, double salarioBase, int horasTrabajadas, double tarifaHora, String departamento) {
+    public Empleado(String nombre, String genero,double salarioBase, int horasTrabajadas, double tarifaHora, String departamento) {
         this.nombre = nombre;
+        this.genero = genero;
         this.salarioBase = salarioBase;
         this.horasTrabajadas = horasTrabajadas;
         this.tarifaHora = tarifaHora;
         this.departamento = departamento;
     }
 
+    public abstract void imprimirDetalles() {
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Genero: " + genero);
+        System.out.println("Salario: " + salarioBase);
+        System.out.println("Horas trabajadas: " + horasTrabajadas);
+        System.out.println("Departamento: " + departamento);
+    }
+
+
     public double calcularSalario() {
-        double salarioTotal = salarioBase;
-        if (salarioBase>0) {
-            if (horasTrabajadas >= 0) {
-                // Horas trabajadas normales = 40;
-                if (horasTrabajadas > 40) {
-                    salarioTotal += (horasTrabajadas - 40) * 50; // Pago de horas extra
-                }
-            }else {
-                throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
-            }
-        } else {
-            throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
-        }
-        switch (departamento) {
-            case "Sistemas":
-                salarioTotal += 20;
-                break;
-            case "Contabilidad":
-                salarioTotal += 10;
-                break;
-            default:
-                break;
-        }
-        return salarioTotal;
+        validarValores();
+        return salarioBase + calcularHorasExtra() + calcularBonoDepartamento();
+    }
+
+    private void validarValores() {
+        if (salarioBase < 0) throw new IllegalArgumentException("El salario debe ser mayor o igual a 0");
+        if (horasTrabajadas < 0) throw new IllegalArgumentException("Las horas trabajadas deben ser mayor o igual a 0");
+    }
+
+    private double calcularHorasExtra() {
+        return horasTrabajadas > 40 ? (horasTrabajadas - 40) * 50 : 0;
+    }
+
+    private double calcularBonoDepartamento() {
+        return switch (departamento) {
+            case "Sistemas" -> 20;
+            case "Contabilidad" -> 10;
+            default -> 0;
+        };
     }
 
     public String getNombre() {
@@ -47,6 +54,14 @@ public class Empleado {
 
     public void setNombre(String nombre) {
         this.nombre = nombre;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
     }
 
     public double getSalarioBase() {
