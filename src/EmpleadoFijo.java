71b83a8b85ffca087d5d807441a1cf08a1152fd4
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-12-11 15:54:14 -0500

    EmpleadoFijo.java

--- src/EmpleadoFijo.java
+++ src/EmpleadoFijo.java
@@ -1,8 +1,7 @@
 public class EmpleadoFijo extends Empleado {
     private double bonoAnual;
-    private String genero;
 
-    public EmpleadoFijo(String nombre, double salarioBase, int horasTrabajadas, String departamento, double bonoAnual) {
+    public EmpleadoFijo(String nombre, double salarioBase, int horasTrabajadas, String departamento, double bonoAnual, String genero) {
         super(nombre, salarioBase, horasTrabajadas, 0, departamento);
         this.bonoAnual = bonoAnual;
         this.genero = genero;
