a18279beabb4fb9f54c6fb2762f74212a64b51ea
JairoBaque <152820015+JairoBaque@users.noreply.github.com>
2024-12-11 16:37:32 -0500

    EmpleadoFijo.java
    método imprimirDetalles se movió a la clase base Empleado

--- src/EmpleadoFijo.java
+++ src/EmpleadoFijo.java
@@ -2,10 +2,14 @@ public class EmpleadoFijo extends Empleado {
     private double bonoAnual;
 
     public EmpleadoFijo(String nombre, double salarioBase, int horasTrabajadas, String departamento, double bonoAnual, String genero) {
-        super(nombre, salarioBase, horasTrabajadas, 0, departamento);
+        super(nombre, salarioBase, horasTrabajadas, departamento, genero);
         this.bonoAnual = bonoAnual;
-        this.genero = genero;
     }
+
+    //Pull Up Method
+    //En el caso del método imprimirDetalles, cada subclase (EmpleadoFijo, EmpleadoPorHoras, EmpleadoTemporario) tenía una versión similar del método. 
+    //Este se movió a la clase base Empleado, ya que gran parte de la lógica era común entre las subclases.
 
     // Más metodos
 }
+
